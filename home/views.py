8083d35ce3c65546c76a6df78732d49125773eeb
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2026-01-24 10:11:24 +0200

    Add search button logic

--- home/views.py
+++ home/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 
-from favorites.favorites import get_count_of_favorite_products, get_favorite_products
+from favorites.favorites import get_favorite_products
 from products.models import Product
 
 def index(request, filter_by_favorites=False):
     products = Product.objects.all()
 
+    filter_text = request.GET.get("filter_text", "")
+
     if filter_by_favorites:
         fav_ids = get_favorite_products(request)
         products = products.filter(id__in=fav_ids)
+
+    if filter_text:
+        products = products.filter(name__icontains=filter_text)
 
     return render(request, 'home/index.html', {
         'products': products, 
